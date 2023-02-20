bdae322134e575572e96c734fe3b03fffd64f6a8
Haitham <sinaw@umich.edu>
2023-02-20 17:56:50 -0500

    added comments.

--- ping.sh
+++ ping.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$1" = "" ]
-then
+then #check for first argument
     echo "IP address not found"
 else
-    for i in `seq 1 254`; do
+    for i in `seq 1 254`; do #ping allhost in the subnet and display only live host
         ping -c 1 $1.$i | grep "64 bytes" | cut -d " " -f4 | sed 's/.$//'
     done
 fi
